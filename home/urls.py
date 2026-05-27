7cb56d807d4ce16f12d7f6219c599fc75f133819
jhonatae <jhonata.conceicao@aluno.ufop.edu.br>
2026-05-27 12:21:21 -0300

    feat: finaliza roteiro parte 2 - rotas, views e pag sobre

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("", include("home.urls")),
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,6 @@ from .models import Mensagem
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+
+def sobre(request):                                   # atualizado para parte 2
+    return render(request, "home/sobre.html")
