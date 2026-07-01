c8f8b2b99ef2f912ed2d2ed9c7c722adbaa2ac16
jhonatae <jhonata.conceicao@aluno.ufop.edu.br>
2026-07-01 14:23:48 -0300

    feat: implementacao roteiro 6

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre'),
     path("nova/", views.nova_mensagem, name="nova_mensagem"),   # ← novo
+    path("mensagens/<int:id>/editar/", views.editar_mensagem, name="editar_mensagem"),   # ← novo
+    path("mensagens/<int:id>/remover/", views.remover_mensagem, name="remover_mensagem"),  # ← novo
 ]
